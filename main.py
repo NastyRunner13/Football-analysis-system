70bed4238077b5420f2d1c0283cbd5a2ea451331
Prince Gupta <princegupta1586@gmail.com>
2024-10-02 22:49:29 +0530

    Update

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from utils import read_video, save_video
 from trackers import Tracker
+from team_assigner import TeamAssigner
+from player_ball_assigner import PlayerBallAssigner
+import cv2
 
 def main():
     
@@ -11,6 +14,42 @@ def main():
                                        read_from_stub=True,
                                        stub_path='stubs/tracker.pkl')
     
+    tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
+    
+    # for track_id, player in tracks['players'][0].items():
+    #     bbox = player['bbox']
+    #     frame = video_frames[0]
+
+    #     cropped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]): int(bbox[2])]
+
+    #     cv2.imwrite("Output_video/cropped_image.jpg", cropped_image)
+
+    #     break
+
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0],
+                                    tracks['players'][0])
+    
+    for frame_num, player_track in enumerate(tracks['players']):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(
+                video_frames[frame_num],
+                track['bbox'],
+                player_id
+            )
+            tracks['players'][frame_num][player_id]['team'] = team
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+
+    
+    player_ball_assigner = PlayerBallAssigner()
+    for frame_num, player_track in enumerate(tracks['players']):
+        ball_bbox = tracks["ball"][frame_num][1]['bbox']
+        assigned_player = player_ball_assigner.assign_ball_to_player(player_track, ball_bbox)
+        
+        if assigned_player != -1:
+            tracks['players'][frame_num][assigned_player]['has_ball'] = True
+    
+
     output_video_frames = tracker.draw_annotations(video_frames, tracks)
 
     save_video(output_video_frames, 'Output_video/output_video.avi')
